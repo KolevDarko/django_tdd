cb0773b52a40f16afa31be1fe814e73d5c1be1ca
KolevDarko <kolevdarko@gmail.com>
2015-09-05 18:33:20 +0200

    First passing test of root ulr mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
-# Create your tests here.
-class SmokeTest(TestCase):
-    def test_math(self):
-        self.assertEquals(2,3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
