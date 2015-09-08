fd6ed48fa00162db392062d329c5b93b985eb542
KolevDarko <kolevdarko@gmail.com>
2015-09-08 05:04:57 +0200

    REdirect after POST, and shows all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -25,11 +25,35 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'})
-        self.assertEquals(response.content.decode(), expected_html)
-        self.assertIn('A new list item', response.content.decode())
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['new_item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_saves_items_when_necessary(self):
+        request = HttpRequest()
+        request.method = 'GET'
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text="yesman")
+        Item.objects.create(text="yesno")
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn("yesman", response.content.decode())
+        self.assertIn("yesno", response.content.decode())
 
 
 class ItemModelTest(TestCase):
