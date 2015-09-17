e714f23cc3bca93fddc67a50607547a74065c572
KolevDarko <kolevdarko@gmail.com>
2015-09-17 07:16:32 +0200

    New form for existing list item and tests for it

--- lists/forms.py
+++ lists/forms.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import ValidationError
 from django import forms
 from lists.models import Item
 
 
 EMPTY_LIST_ERROR = "Cannot have empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 
 class ItemForm(forms.models.ModelForm):
@@ -23,3 +25,21 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_LIST_ERROR}
         }
+
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    # is_valid to work, I need to override validate_unique
+    # that's one way though, I guess
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            # Take the error and set our custom message
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            # Then insert it back in django
+            self._update_errors(e)
