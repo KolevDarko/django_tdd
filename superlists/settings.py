84d9a27daa24ac5fa7b43725b1a248a6f0a41590
KolevDarko <kolevdarko@gmail.com>
2015-10-02 20:17:26 +0200

    added logging again

--- superlists/settings.py
+++ superlists/settings.py
@@ -113,3 +113,19 @@ AUTH_USER_MODEL = 'accounts.User'
 AUTHENTICATION_BACKENDS = (
     'accounts.authentication.PersonaAuthenticationBackend',
 )
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                },
+            },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                },
+            },
+        'root': {'level': 'INFO'},
+        }
